7743fd95d8f2e452a8c6224a644b39f24698335f
Donna <skies09@hotmail.com>
2018-08-14 08:58:44 +0000

    initial commit

---
+++ js/myscript.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('h2').addClass('underline');
+    $('ul').addClass('border');
+});
+
+ $(document).ready(function(){
+    $("tr:odd").addClass("selection");
+  });
+  
+   $(document).ready(function(){
+    $("tr:even").addClass("selection-2");
+  });
